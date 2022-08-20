8fac39aa857ce37de8227439f7858415a5492799
ivan-mitriakhin <imitriakhin@gmail.com>
2022-08-20 13:43:29 +0400

    Implemented basic functionality, such as pads producing sounds, switching banks and power, volume slider and a display.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,99 @@
 import './App.css';
 import React from 'react';
+import { bankOne, bankTwo } from './banksConst.js';
 
 function Button(props) {
+  React.useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => { document.removeEventListener('keydown', handleKeyPress) };
+  });
+
+  const handleKeyPress = (event) => {
+    if (event.keyCode === props.keyCode) {
+      playSound();
+    }
+  }
+
+  const playSound = () => {
+    const sound = document.getElementById(props.keyTrigger);
+    sound.currentTime = 0;
+    sound.play();
+    activateButton(sound);
+    deactivateButton(sound);
+    props.updateDisplay(props.clipId)
+  }
+
+  const activateButton = (sound) => {
+    sound.parentElement.style.backgroundColor = 'blue';
+  }
+
+  const deactivateButton = (sound) => {
+    setTimeout(() => { 
+      sound.parentElement.style.backgroundColor = 'rgb(27, 26, 26)' 
+    }, 100);
+  }
   
   return (
-    <div className="drumPad">
+    <div className="drumPad" id={props.clipId} onClick={playSound}>
+      <audio className="clip" id={props.keyTrigger} src={props.clip}/>
       {props.keyTrigger}
     </div>
   );
 }
 
-function Pad() {
+function Pad(props) {
+  let buttons;
+  if (props.power) {
+    buttons = props.currentPadBank.map((drumObj) => {
+      return <Button
+                clip={drumObj.src}
+                clipId={drumObj.id}
+                keyCode={drumObj.keyCode}
+                keyTrigger={drumObj.keyTrigger}
+                power={props.power}
+                updateDisplay={props.updateDisplay}
+              />
+    });
+  } else {
+    buttons = props.currentPadBank.map((drumObj) => {
+      return <Button
+                clip='#'
+                clipId={drumObj.id}
+                keyCode={drumObj.keyCode}
+                keyTrigger={drumObj.keyTrigger}
+                power={props.power}
+                updateDisplay={props.updateDisplay}
+              />
+    });
+  }
+
   return (
     <div className="pad">
-      <Button keyTrigger='Q'/>
-      <Button keyTrigger='W'/>
-      <Button keyTrigger='E'/>
-      <Button keyTrigger='A'/>
-      <Button keyTrigger='S'/>
-      <Button keyTrigger='D'/>
-      <Button keyTrigger='Z'/>
-      <Button keyTrigger='X'/>
-      <Button keyTrigger='C'/>
+      {buttons}
     </div>
   );
 }
 
-function ControlPanel() {
+function ControlPanel(props) {
+
   return (
     <div className="controlPanel">
       <div className="switcher">
         <p>Power</p>
-        <div className="select">
-          <div className="inner"></div>
+        <div className="select" onClick={props.powerControl}>
+          <div className="inner" style={props.powerSlider}></div>
         </div>
       </div>
       <p className="display">
-        
+        <span>{props.display}</span>
       </p>
       <div className="volumeSlider">
-        <input type="range" step="0.01" min="0" max="1" />
+        <input type="range" step="0.01" min="0" max="1" onChange={props.adjustVolume} value={props.sliderVal}/>
       </div>
       <div className="switcher">
         <p>Bank</p>
-        <div className="select">
-          <div className="inner"></div>
+        <div className="select" onClick={props.selectBank}>
+          <div className="inner" style={props.bankSlider}></div>
         </div>
       </div>
     </div>
@@ -52,11 +101,73 @@ function ControlPanel() {
 }
 
 function App() {
+  let [power, setPower] = React.useState(true);
+  let [display, setDisplay] = React.useState(String.fromCharCode(160));
+  let [currentPadBank, setCurrentPadBank] = React.useState(bankOne);
+  let [currentPadBankId, setCurrentPadBankId] = React.useState("Basement Kit");
+  let [sliderVal, setSliderVal] = React.useState(0.3);
+
+  const powerSlider = power ? { float: 'right' } : { float: 'left' };
+  const bankSlider = currentPadBank === bankOne ? { float: 'left' } : { float: 'right' };
+  const clips = [].slice.call(document.getElementsByClassName('clip'));
+  clips.forEach(sound => {
+    sound.volume = sliderVal;
+  });
+
+  const powerControl = () => {
+    setPower(!power);
+    setDisplay(String.fromCharCode(160));
+  }
+  
+  const selectBank = () => {
+    if (power) {
+      if (currentPadBankId === "Basement Kit") {
+        setCurrentPadBank(bankTwo);
+        setCurrentPadBankId("Modern Kit");
+        setDisplay("Modern Kit");
+      } else {
+        setCurrentPadBank(bankOne);
+        setCurrentPadBankId("Basement Kit");
+        setDisplay("Basement Kit");
+      }
+    }
+  }
+
+  const displayClipName = (name) => {
+    if (power) {
+      setDisplay(name);
+    }
+  }
+
+  const adjustVolume = (event) => {
+    if (power) {
+      setSliderVal(event.target.value);
+      setDisplay('Volume: ' + Math.round(event.target.value * 100));
+      setTimeout(() => clearDisplay(), 1000);
+    }
+  }
+
+  const clearDisplay = () => {
+    setDisplay(String.fromCharCode(160));
+  }
 
   return (
     <div id="drumMachine">
-      <Pad />
-      <ControlPanel />
+      <Pad 
+        power={power}
+        currentPadBank={currentPadBank}
+        clipVolume={sliderVal}
+        updateDisplay={displayClipName}
+      />
+      <ControlPanel 
+        powerControl={powerControl} 
+        powerSlider={powerSlider} 
+        display={display} 
+        adjustVolume={adjustVolume}
+        sliderVal={sliderVal}
+        selectBank={selectBank}
+        bankSlider={bankSlider}
+      />
     </div>
   );
 }
---
+++ src/banksConst.js
@@ -0,0 +1,114 @@
+const bankOne = [
+    {
+        keyCode: 81,
+        keyTrigger: 'Q',
+        id: 'percussion1',
+        src: '/audio/bank1/percussion1.wav' 
+    },
+    {
+        keyCode: 87,
+        keyTrigger: 'W',
+        id: 'percussion2',
+        src: '/audio/bank1/percussion2.wav'
+    },
+    {
+        keyCode: 69,
+        keyTrigger: 'E',
+        id: 'openHat',
+        src: '/audio/bank1/openhat.wav'
+    },
+    {
+        keyCode: 65,
+        keyTrigger: 'A',
+        id: 'mediumTom',
+        src: '/audio/bank1/mediumtom.wav'
+    },
+    {
+        keyCode: 83,
+        keyTrigger: 'S',
+        id: 'softTom',
+        src: '/audio/bank1/softtom.wav'
+    },
+    {
+        keyCode: 68,
+        keyTrigger: 'D',
+        id: 'hiHat',
+        src: '/audio/bank1/hihat.wav'
+    },
+    {
+        keyCode: 90,
+        keyTrigger: 'Z',
+        id: 'kicknHat',
+        src: '/audio/bank1/kicknhat.wav'
+    },
+    {
+        keyCode: 88,
+        keyTrigger: 'X',
+        id: 'kick',
+        src: '/audio/bank1/kick.wav'
+    },
+    {
+        keyCode: 67,
+        keyTrigger: 'C',
+        id: 'snare',
+        src: '/audio/bank1/snare.wav'
+    }
+];
+
+const bankTwo = [
+    {
+        keyCode: 81,
+        keyTrigger: 'Q',
+        id: 'chime',
+        src: '/audio/bank2/chime.wav' 
+    },
+    {
+        keyCode: 87,
+        keyTrigger: 'W',
+        id: 'rattler',
+        src: '/audio/bank2/rattler.wav'
+    },
+    {
+        keyCode: 69,
+        keyTrigger: 'E',
+        id: 'openHat',
+        src: '/audio/bank2/openhat.wav'
+    },
+    {
+        keyCode: 65,
+        keyTrigger: 'A',
+        id: 'bells',
+        src: '/audio/bank2/bells.wav'
+    },
+    {
+        keyCode: 83,
+        keyTrigger: 'S',
+        id: 'rim',
+        src: '/audio/bank2/rim.wav'
+    },
+    {
+        keyCode: 68,
+        keyTrigger: 'D',
+        id: 'hiHat',
+        src: '/audio/bank2/hihat.wav'
+    },
+    {
+        keyCode: 90,
+        keyTrigger: 'Z',
+        id: 'shaker',
+        src: '/audio/bank2/shaker.wav'
+    },
+    {
+        keyCode: 88,
+        keyTrigger: 'X',
+        id: 'kick',
+        src: '/audio/bank2/kick.wav'
+    },
+    {
+        keyCode: 67,
+        keyTrigger: 'C',
+        id: 'clap',
+        src: '/audio/bank2/clap.wav'
+    }
+];
+export { bankOne, bankTwo };
